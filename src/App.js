492231ccfaa2c78ea6f425d8ca1f123a4231713b
Annamarie San Diego <annamarie.sandiego@gmail.com>
2023-10-07 19:42:59 +0800

    Added cart modal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Header from './components/Layout/Header';
 import Treats from './components/Treats/Treats';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Treats />
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,30 @@
+import classes from './Modal.module.css';
+import ReactDOM from 'react-dom';
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModelOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModelOverlay>{props.children}</ModelOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
